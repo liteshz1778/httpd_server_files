ed03403d1054a145cf4527f84d7d5aeb66ffca48
Litesh Zadane <100953302+liteshz1778@users.noreply.github.com>
2026-04-24 14:36:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,19 +4,11 @@
 sudo cp -rf * /var/www/html
 
 # Generate HTML
-cat <<EOF > /var/www/html/index.html
+cat <<EOF > /var/www/html/script_output/index.html
 <html>
 <body>
-
 <h2>Deployment Info</h2>
-
-<p>Hi, this is $Name & work as $Work</p>
-
 <p>Code deployed on $(hostname -i) server</p>
-
-<p>Job Name: $JOB_NAME</p>
-<p>Build ID: $BUILD_ID</p>
-
 </body>
 </html>
 EOF
