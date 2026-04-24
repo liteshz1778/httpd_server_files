2be8bdf4f95f4a694eb215dd0f5170a520d2daa1
= <=>
2026-04-24 04:15:57 -0400

    Added changes related to script for accessing built-in & user-defined ENV Variables - 24/04/2026

--- script.sh
+++ script.sh
@@ -1,2 +1,19 @@
-sudo cp -rvf * /var/www/html
+# accessing User-Defined ENV Variables
+echo "Hi, this is  {Name} &  work as {Work}"
+
+echo -e "\n"
+
+# Copy files to /var/www/html directory
+sudo cp -rf * /var/www/html
+# Not used -v flag for verbose
+
+echo -e "\n"
+
+# accessing server
 echo "Code deployed on $(hostname -i) server"
+
+echo -e "\n"
+
+# accessing built-in ENV Variables
+echo "Job Name: $JOB_NAME" 
+echo "Build ID: $BUILD_ID"
