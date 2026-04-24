1b58456fa094a05e55564ece5c29454d03c03891
= <=>
2026-04-24 06:40:07 -0400

    Fixed script.sh - 24/04/2026

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ sudo chmod 777 /var/www/html/script_output/index.html
 # Generate HTML
 sudo cat <<EOF > /var/www/html/script_output/index.html
 <html>
-<body>
+<body style="background-color: #e6ffe6;">
 <h2>Deployment Info</h2>
 <p>Code deployed on $(hostname -i) server </p>
 <a href="../">Home Page</a>
