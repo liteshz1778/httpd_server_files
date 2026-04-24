24b1594ac9c800d84bf878f553c1db40952acf5c
= <=>
2026-04-24 06:26:36 -0400

    change in script.sh04 - 24/04/2026

--- script.sh
+++ script.sh
@@ -9,15 +9,8 @@ sudo cat <<EOF > ./script_output/index.html
 <body>
 <h2>Deployment Info</h2>
 <p>Code deployed on $(hostname -i) server </p>
-</body>
-</html>
-EOF
-
-sudo cat <<EOF >> ./script_output/index.html
-<html>
-<body>
-<h2>Deployment Info</h2>
-<p><a href="../">Home Page </a></p>
+<br>
+<a href="../index.html">Home Page</a>
 </body>
 </html>
 EOF
