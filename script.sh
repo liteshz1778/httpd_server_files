b13c3f5bee0cf7903517575b3c124a6059f7d473
= <=>
2026-04-24 06:21:18 -0400

    change in script.sh03 - 24/04/2026

--- script.sh
+++ script.sh
@@ -8,8 +8,16 @@ sudo cat <<EOF > ./script_output/index.html
 <html>
 <body>
 <h2>Deployment Info</h2>
-<p>Code deployed on $(hostname -i) server>
-<a href="../">Home Page</a></p>
+<p>Code deployed on $(hostname -i) server </p>
+</body>
+</html>
+EOF
+
+sudo cat <<EOF >> ./script_output/index.html
+<html>
+<body>
+<h2>Deployment Info</h2>
+<p><a href="../">Home Page </a></p>
 </body>
 </html>
 EOF
