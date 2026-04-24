8a0cd0d565687fc687684c15291d0245feb67e77
= <=>
2026-04-24 06:16:01 -0400

    change in script.sh - 24/04/2026

--- script.sh
+++ script.sh
@@ -8,9 +8,9 @@ sudo cat <<EOF > ./script_output/index.html
 <html>
 <body>
 <h2>Deployment Info</h2>
-<p>Code deployed on $(hostname -i) server</p>
+<p>Code deployed on $(hostname -i) server>
 <br>
-<a href="../">Home Page</a>
+<a href="../">Home Page</a></p>
 </body>
 </html>
 EOF
