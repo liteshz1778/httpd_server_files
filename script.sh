3144248e32a6b9b5401bfc9c80b41bee1a1351af
root <root@ip-172-31-20-185.ec2.internal>
2026-06-12 18:50:08 +0000

    Added changes to index.html & script.sh & .dockerignore file - 13/06/2026

--- script.sh
+++ script.sh
@@ -2,13 +2,43 @@
 
 touch ./script_output/index.html
 
-# Generate HTML 
 cat <<EOF > ./script_output/index.html
 <html>
 <body style="background-color: #e6ffe6;">
 <h2>Deployment Info</h2>
-<p>Code deployed on $(hostname -i) server </p>
+
+<table border="1" cellpadding="5">
+
+<tr>
+  <th>Property</th>
+  <th>Value</th>
+</tr>
+
+<tr>
+  <td>Hostname</td>
+  <td>$(hostname)</td>
+</tr>
+
+<tr>
+  <td>IP Address</td>
+  <td>$(hostname -i)</td>
+</tr>
+
+<tr>
+  <td>Date</td>
+  <td>$(date)</td>
+</tr>
+
+<tr>
+  <td>ENV Variables</td>
+  <td><pre>$(env)</pre></td>
+</tr>
+
+</table>
+
+<br>
 <a href="../">Home Page</a>
+
 </body>
 </html>
 EOF
